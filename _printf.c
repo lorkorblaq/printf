53f9bc03295745c57169e9cc451056804875ccf2
Olorunfemi Oloko <91541728+lorkorblaq@users.noreply.github.com>
2023-03-29 01:28:09 +0100

    Update and rename _print.c to _printf.c

--- _printf.c
+++ _printf.c
@@ -12,11 +12,11 @@ int _printf(const char *format, ...)
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
+
 	va_start(list, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
@@ -44,11 +44,13 @@ int _printf(const char *format, ...)
 			printed_chars += printed;
 		}
 	}
+
 	print_buffer(buffer, &buff_ind);
+
 	va_end(list);
+
 	return (printed_chars);
 }
-
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
@@ -59,5 +61,6 @@ void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
+
 	*buff_ind = 0;
 }
